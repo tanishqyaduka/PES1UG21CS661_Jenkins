34d41fec40b2af59284423ad1b9bb7e9078b703a
Tanishq Yaduka <114014875+tanishqyaduka@users.noreply.github.com>
2024-03-06 20:32:22 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World (PES1UG21CS661)\n";
+  cout << "Hello, Tanishq (PES1UG21CS661)\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
